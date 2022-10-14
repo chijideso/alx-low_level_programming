7853c87f5b28305d6a2696a894dbadef3ae12ef3
root <chijideso@gmail.com>
2022-10-14 04:15:08 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,9 @@
  *
  */
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	char test_str[] = ("\"Programming is like building a multilingual puzzle";
+
+	puts(test_str);
 	return (0);
