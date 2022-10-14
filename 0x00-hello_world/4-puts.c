ee2adea1616935613c462c58c5b6ceab546862cb
root <chijideso@gmail.com>
2022-10-14 06:53:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
+ *
  *main - main block
  * 
  Return: 0 (Success)
  *
  */
-
 int main(void)
 {
-		char test_str[] = "\"Programming is like building a multilingual puzzle";
-	puts(test_str);
-	return (0);
+char test_str[] = "\"Programming is like building a multilingual puzzle";
+puts(test_str);
+return (0);
 
 }
