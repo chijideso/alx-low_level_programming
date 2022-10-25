2795c5b4da2ae9a5e0e167dd38d5a1fada1042a5
chijideso <chijideso@gmail.com>
2022-10-25 07:07:55 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+es (15 sloc)  186 Bytes
+
+#include "main.h"
+/**
+ * _strlen - string lenght
+ * @s: params
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
